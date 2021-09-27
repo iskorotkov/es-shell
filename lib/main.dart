b7c3dffe576d0a1a9719bd75dd79ee35aa028f67
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-09-27 18:39:43 +0500

    chore: rename MyApp to App

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'widgets/rules/rules_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
